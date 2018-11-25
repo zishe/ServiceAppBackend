67d729725258eaa2fb1fa4042696b9a3a0658df1
Alexander Kireev <farvistadw@gmail.com>
2018-11-25 21:00:43 +0400

    fixed

--- app/graphql/rails_backend_schema.rb
+++ app/graphql/rails_backend_schema.rb
@@ -1,4 +1,4 @@
 class RailsBackendSchema < GraphQL::Schema
-  mutation(Types::MutationType)
-  query(Types::QueryType)
+  mutation Types::MutationType
+  query Types::QueryType
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,10 +2,15 @@ module Types
   class QueryType < Types::BaseObject
     graphql_name "RootQuery"
 
-    field :categories, [Types::CategoryType], null: false
+    field :categories, [CategoryType], null: false
+    field :products, [ProductType], null: false
 
     def categories
       Category.all
+    end
+
+    def products
+      Product.all
     end
   end
 end
